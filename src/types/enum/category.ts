3111946a564396c6ff3b642f228339453818d490
roee ltmanovich <roeeli770@gmail.com>
2024-11-20 17:26:20 +0200

    creted enum for  category

---
+++ src/types/enum/category.ts
@@ -0,0 +1,8 @@
+export enum category {
+    Fruits = "Fruits",
+    Vegetables = "Vegetables",
+    Grains = "Grains",
+    Dairy = "Dairy",
+    Nuts_and_Seeds = "Nuts_and_Seeds",
+    Sweets = "Sweets",
+}
