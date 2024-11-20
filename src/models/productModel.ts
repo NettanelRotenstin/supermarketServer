41201f071a06fb6a284050f1cd0389767af98137
roee ltmanovich <roeeli770@gmail.com>
2024-11-20 17:26:45 +0200

    created model for product

---
+++ src/models/productModel.ts
@@ -0,0 +1,38 @@
+import { Document, Model, model, Schema } from "mongoose";
+import { category } from "../types/enum/category";
+
+export interface IProduct extends Document {
+    name: string;
+    image?: string
+    category: category; // need to create enum
+    price: number;
+    quantity: number;
+}
+
+export const productSchema = new Schema<IProduct>({
+    name: {
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: false
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: [category.Dairy, category.Fruits, category.Grains, category.Nuts_and_Seeds, category.Sweets, category.Vegetables]
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+    },
+});
+
+const ProductModel: Model<IProduct> = model<IProduct>("Product", productSchema)
+
+export default ProductModel
