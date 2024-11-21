d7d3912e56bedd5471578e09cf78fc6edba39cc1
ruviRedner <r.y.r7966@gmail.com>
2024-11-21 10:17:33 +0200

    change

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,10 +1,10 @@
 import CartModel from "../models/cartModel";
 
 export const cancelCartService = async (id: string) => {
-    try {
-        const result = await CartModel.findByIdAndDelete(id);
-        return result;
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    const result = await CartModel.findByIdAndDelete(id);
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
