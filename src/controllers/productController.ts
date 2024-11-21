14bb279dae4caaecdf63162afaf883f005053b41
roee ltmanovich <roeeli770@gmail.com>
2024-11-21 12:36:59 +0200

    create 2 function in controller for get all product and get product by Category

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,9 +1,41 @@
 import { Request, Response } from "express";
-import { getByNameService } from "../services/productService";
+import { getByNameService, getAllProdutsService, getProdutsByCategoryService } from "../services/productService";
 
 export const getProductByName = async (req: Request, res: Response):Promise<void> => {
     try {
         const result = await getByNameService(req.params.name)
+        res.status(201).json({
+            msg: "user login succesfull",
+            err: false,
+            data:result
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: "something went wrong",
+            err: true,
+            data:null
+        });
+    }
+};
+export const getProdutsByCategory = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const result = await getProdutsByCategoryService(req.params.name)
+        res.status(201).json({
+            msg: "user login succesfull",
+            err: false,
+            data:result
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: "something went wrong",
+            err: true,
+            data:null
+        });
+    }
+};
+export const getAllProducts = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const result = await getAllProdutsService()
         res.status(201).json({
             msg: "user login succesfull",
             err: false,
