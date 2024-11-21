4ee06fb08121d25778290ea1a80cfd687a3ef7e7
roee ltmanovich <roeeli770@gmail.com>
2024-11-21 12:37:35 +0200

    connect router in service

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { getProdutsByCategory, getAllProducts } from "../controllers/productController";
 
 const router = Router();
 
-router.get("/:name",()=>{});
+router.get("/search/:name",()=>{});
 
-router.get("/", ()=>{})
+router.get("/:category", getProdutsByCategory)
+
+router.get("/", getAllProducts)
 
 
 export default router;
