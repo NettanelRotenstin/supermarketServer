56bb0c9b06d253be7231ec05b4ed80254ca48213
ruviRedner <r.y.r7966@gmail.com>
2024-11-21 11:02:52 +0200

    Scrum-26-add product done

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,7 @@
+import { Types } from "mongoose";
 import CartModel from "../models/cartModel";
+import ProductModel from "../models/productModel";
+import { CartDto } from "../types/dto/cartDto";
 
 export const cancelCartService = async (id: string) => {
   try {
@@ -6,5 +9,50 @@ export const cancelCartService = async (id: string) => {
     return result;
   } catch (error) {
     return error;
+  }
+};
+
+export const addToCart = async (Cart: CartDto): Promise<void> => {
+  try {
+    const product = await ProductModel.findOne({ name: Cart.prodactName });
+    if (!product) {
+      throw new Error("No such product exists.");
+    }
+
+    if (product.quantity < Cart.quantity) {
+      throw new Error(
+        "The product is out of stock or insufficient quantity available."
+      );
+    }
+
+    const newItem = {
+      idproduct: product._id as Types.ObjectId,
+      quantity: Cart.quantity,
+      price: product.price,
+    };
+
+    let cart = await CartModel.findOne({ user_id: Cart.userId, isPaid: false });
+
+    if (!cart) {
+      cart = new CartModel({
+        user_id: Cart.userId,
+        totalPrice: 0,
+        receipt: [],
+        isPaid: false,
+        date: new Date(),
+      });
+    }
+
+    cart.receipt.push(newItem);
+
+    cart.totalPrice += product.price * Cart.quantity;
+
+    await cart.save();
+
+    product.quantity -= Cart.quantity;
+    await product.save();
+  } catch (error) {
+    console.error("Error adding to cart:", error);
+    throw error;
   }
 };
