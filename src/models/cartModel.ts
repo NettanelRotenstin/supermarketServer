c98318d8001732ec93ab7f02cc8715b7085bbfec
ruviRedner <r.y.r7966@gmail.com>
2024-11-21 12:17:30 +0200

    add defolt for date

--- src/models/cartModel.ts
+++ src/models/cartModel.ts
@@ -1,41 +1,42 @@
 import mongoose, { Document, Model, model, Schema, Types } from "mongoose";
 
 export interface ICart extends Document {
-    user_id: Types.ObjectId;
-    totalPrice: number;
-    receipt: [{ idproduct: Types.ObjectId; quantity: number; price: number }];
-    isPaid: boolean;
-    date: Date;
+  user_id: Types.ObjectId;
+  totalPrice: number;
+  receipt: [{ idproduct: Types.ObjectId; quantity: number; price: number }];
+  isPaid: boolean;
+  date: Date;
 }
 
 export const cartSchema = new Schema<ICart>({
-    user_id: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
-    },
-    totalPrice: {
-        type: Number,
-        required: true,
-    },
-    receipt: {
-        type: [
-            {
-                idproduct: { type: Schema.Types.ObjectId, ref: "Product" },
-                quantity: Number,
-                price: Number,
-            },
-        ],
-        required: true,
-    },
-    isPaid: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
-    date: {
-        type: Date
-    },
+  user_id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  receipt: {
+    type: [
+      {
+        idproduct: { type: Schema.Types.ObjectId, ref: "Product" },
+        quantity: Number,
+        price: Number,
+      },
+    ],
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const CartModel: Model<ICart> = model<ICart>("Cart", cartSchema);
