cc75bf108a55bd754c9dbb80530833816f8c5628
roee ltmanovich <roeeli770@gmail.com>
2024-11-24 14:31:10 +0200

    returen getByNameService to regex

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,27 +1,30 @@
 import ProductModel, { productSchema } from "../models/productModel";
-import productsForCeed from '../db/products.json'
+import productsForCeed from "../db/products.json";
 
 export const getByNameService = async (name: string) => {
     try {
-        const result = await ProductModel.find({ name: name }).lean();
+        const result = await ProductModel.find({ name: name }).where("name").regex(name).lean();        
+        
         return result;
     } catch (error) {
         return error;
     }
 };
 
-export const getAllProdutsService  = async () => {
+export const getAllProdutsService = async () => {
     try {
-        const result = await ProductModel.find({}).lean();        
+        const result = await ProductModel.find({}).lean();
         return result;
     } catch (error) {
         return error;
     }
 };
-export const getProdutsByCategoryService  = async (nameOfCategory: string) => {
+export const getProdutsByCategoryService = async (nameOfCategory: string) => {
     try {
-        const result = await ProductModel.find({category: nameOfCategory}).lean();
-        return result
+        const result = await ProductModel.find({
+            category: nameOfCategory,
+        }).lean();
+        return result;
     } catch (error) {
         return error;
     }
@@ -29,14 +32,14 @@ export const getProdutsByCategoryService  = async (nameOfCategory: string) => {
 
 export const ceed = async () => {
     try {
-        const productCount = await ProductModel.countDocuments()
-        if(productCount == 0){
-        productsForCeed.map(async (prod) => {
-            const newProd = new ProductModel(prod);
-            await newProd.save();
-        
-        })}
+        const productCount = await ProductModel.countDocuments();
+        if (productCount == 0) {
+            productsForCeed.map(async (prod) => {
+                const newProd = new ProductModel(prod);
+                await newProd.save();
+            });
+        }
     } catch (error) {
-        console.log("error in ceed")
+        console.log("error in ceed");
     }
-}
+};
