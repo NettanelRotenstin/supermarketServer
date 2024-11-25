f77fc82616d708cba085d4edf97e1062710db63b
ruviRedner <r.y.r7966@gmail.com>
2024-11-25 14:11:15 +0200

    change to io

--- src/socket/io.ts
+++ src/socket/io.ts
@@ -25,10 +25,10 @@ export const handelSocketConnection = async (client: Socket) => {
   client.on("get-allProducts", async (id) => {
     try {
       const products = await getProductById(id);
-      client.emit("allProducts", products);
+      io.emit("allProducts", products);
     } catch (error) {
       console.error("Error fetching all products:", error);
-      client.emit("error", "Failed to fetch products.");
+      io.emit("error", "Failed to fetch products.");
     }
   });
   client.on("productsByCategory", async (data) => {
@@ -57,39 +57,39 @@ export const handelSocketConnection = async (client: Socket) => {
     try {
       await decreaseQuantity(data);
       const updatedCart = await getActionById(data.userId);
-      client.emit("cartUpdated", updatedCart);
+      io.emit("cartUpdated", updatedCart);
     } catch (error) {
       console.error("Error decreasing quantity:", error);
-      client.emit("error", "Failed to decrease quantity.");
+      io.emit("error", "Failed to decrease quantity.");
     }
   });
   client.on("removeItem", async (data) => {
     try {
       await removeFromCart(data);
       const updatedCart = await getActionById(data.userId);
-      client.emit("cartUpdated", updatedCart);
+      io.emit("cartUpdated", updatedCart);
     } catch (error) {
       console.error("Error removing item:", error);
-      client.emit("error", "Failed to remove item from cart.");
+      io.emit("error", "Failed to remove item from cart.");
     }
   });
   client.on("clearAllCart", async (userId) => {
     try {
       await cancelCartService(userId);
       const updatedCart = await getActionById(userId);
-      client.emit("cartUpdated", updatedCart);
+      io.emit("cartUpdated", updatedCart);
     } catch (error) {
       console.error("Error clearing cart:", error);
-      client.emit("error", "Failed to clear cart.");
+      io.emit("error", "Failed to clear cart.");
     }
   });
   client.on("getCart", async (userId) => {
     try {
       const cart = await getActionById(userId);
-      client.emit("cartUpdated", cart);
+      io.emit("cartUpdated", cart);
     } catch (error) {
       console.error("Error fetching cart:", error);
-      client.emit("error", "Failed to fetch cart.");
+      io.emit("error", "Failed to fetch cart.");
     }
   });
   client.on('history', async (userId: HistoryDto) => {
